d7ff0cbfb799087bcd8287f284f30ad13b78b997
Netson Cavina <netsoncavina@gmail.com>
2024-01-22 20:28:23 -0300

    design fix

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,7 +10,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 220,
+      height: 330,
       child: ListView.builder(
           itemCount: transactions.length,
           itemBuilder: (ctx, index) {
