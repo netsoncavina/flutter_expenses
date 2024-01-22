addc00fa4059945b92718de21c39809a6556cfc2
Netson Cavina <netsoncavina@gmail.com>
2024-01-21 21:35:34 -0300

    refatoração

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,37 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
-
-import 'package:expenses/components/transaction_form.dart';
-import 'package:expenses/components/transaction_list.dart';
-import 'package:expenses/models/transaction.dart';
+import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
-main() => runApp(ExpensesApp());
+main() => runApp(const ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
   const ExpensesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key});
-  final _transactions = [
-    Transaction(id: 't1', title: 'Novo Tênis de Corrida', value: 310.76, date: DateTime.now()),
-    Transaction(id: 't2', title: 'Conta de Luz', value: 211.30, date: DateTime.now()),
-  ];
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Despesas Pessoais'),
+          title: const Text('Despesas Pessoais'),
           backgroundColor: Colors.blue[300],
         ),
-        body: Column(
+        body: const Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Card(
-              child: Text('Gráfico'),
               elevation: 5,
+              child: Text('Gráfico'),
             ),
-            TransactionList(transactions: _transactions),
-            TransactionForm(),
+            TransactionUser()
           ],
         ));
   }
